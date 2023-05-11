0de3fe9e897f250cf83da59e14722ae69182c5ca
picocode <38665815+picocode1@users.noreply.github.com>
2023-05-11 15:05:37 +0200

    Add files via upload

---
+++ database.php
@@ -0,0 +1,9 @@
+<?php
+
+// connect to local database
+$serverName = 'localhost';
+$userName = 'root';
+$userPassword = 'vistasql123';
+$dbName = 'testing';
+ 
+$conn = mysqli_connect($serverName, $userName, $userPassword, $dbName);
