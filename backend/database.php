86e2c14379461abfc9ff3276a41b13316ed14acf
picocode <38665815+picocode1@users.noreply.github.com>
2023-05-15 10:55:16 +0200

    pushit

--- backend/database.php
+++ backend/database.php
@@ -4,6 +4,12 @@
 $serverName = 'localhost';
 $userName = 'root';
 $userPassword = 'vistasql123';
+<<<<<<< Updated upstream
 $dbName = 'testing';
+=======
+$dbName = 'duzenco';
+
+$base_link = '/examenproject/backend/';
+>>>>>>> Stashed changes
  
 $conn = mysqli_connect($serverName, $userName, $userPassword, $dbName);
