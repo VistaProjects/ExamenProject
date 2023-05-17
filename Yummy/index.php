7fd8f75556629f533267512a769bec56da1d9511
Jarno Sijben <91888926+ClosingYard@users.noreply.github.com>
2023-05-17 09:41:05 +0200

    Mijn branch werkend test
    adit is de template met functionerende php file. gebruik de link die vermeld is in index php en zet xamp aan

---
+++ Yummy/index.php
@@ -0,0 +1,5 @@
+<?php
+include 'index.html';
+echo '<script src="https://cdn.tiny.cloud/1/vikfeoahythvd97fj1c8efo28ke56n60a5luw2jfot1h490y/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>';
+echo "<script src='nour.js'></script>";
+?>
---
+++ Yummy/nour.js
@@ -0,0 +1,263 @@
+
+
+// Sidebar controlpanel
+var mySection = document.getElementById('hero'),
+  sideButton = document.createElement('div');
+sideButton.innerHTML = `
+<a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+Control sidebar
+</a>
+
+
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel" data-bs-scroll="true">
+<div class="offcanvas-header">
+  <h5 class="offcanvas-title" id="offcanvasExampleLabel">Control Panel</h5>
+  <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+</div>
+<div class="offcanvas-body">
+  <!-- show accrodion -->
+  <div class="accordion" id="accordionPanelsStayOpenExample">
+
+    <!-- Start Editing The Text Nour -->
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="panelsStayOpen-headingFive">
+        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFive" aria-expanded="true" aria-controls="panelsStayOpen-collapseFive">
+          <h6>Edit Text</h6>
+        </button>
+      </h2>
+      <div id="panelsStayOpen-collapseFive" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+        <div class="accordion-body">
+          <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Change the Text</label>
+            
+            <!-- <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder"> -->
+            <textarea type="text" class="form-control" id="mytextarea" placeholder="Example input placeholder" value=""></textarea>
+          </div>
+          <div class="mb-3">
+            <label for="formGroupExampleInput2" class="form-label">Change color</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End Editing The Text Nour -->
+
+    <!-- Start Editing The Text Nour -->
+    
+    <!-- End Editing The Text Nour-->
+
+
+    <!-- Start Editing The Photos-->
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+          <h6>Edit photos</h6>
+        </button>
+      </h2>
+      <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+        <div class="accordion-body">
+          <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        </div>
+      </div>
+    </div>
+    <!-- End Editing The Photos -->
+
+    <!-- Start Editing The Videos-->
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+          <h6>Edit videos</h6>
+        </button>
+      </h2>
+      <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+        <div class="accordion-body">
+          <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        </div>
+      </div>
+    </div>
+    <!-- End Editing The Videos-->
+  </div>
+</div>
+</div>
+`;
+// styleing sidebar
+sideButton.style.zIndex = 10;
+sideButton.style.position = "fixed";
+mySection.appendChild(sideButton);
+mySection.insertBefore(sideButton, mySection.firstElementChild);
+
+
+
+
+// get the sidebar button
+var sideButton = document.querySelector('.btn-primary');
+
+// add an event listener to the button to open the sidebar
+sideButton.addEventListener('click', () => {
+  const offcanvas = new bootstrap.Offcanvas(document.getElementById('offcanvasExample'));
+  offcanvas.show();
+});
+
+// get all the paragraphs and headers on the page
+const elements = document.querySelectorAll('p, h1, h2, h3, h4, h5, h6');
+
+// add an event listener to each element
+elements.forEach(element => {
+  element.addEventListener('click', () => {
+    // add a class to the clicked element
+    element.classList.add('clicked');
+    // remove the class from all the other elements
+    const allElements = document.querySelectorAll('.clicked');
+    allElements.forEach(element => {
+      element.classList.remove('clicked');
+      });
+      console.log(allElements);
+      const text = element.cloneNode(true).textContent;
+    // const text = element.textContent; // get the text content of the clicked element
+    console.log(text)
+    // update the text area in the sidebar with the clicked element's text
+    const sidebarTextArea = document.getElementById('mytextarea');
+    console.log(sidebarTextArea);
+    sidebarTextArea.value = text;
+    sidebarTextArea.innerHTML = `${text}`;
+    sidebarTextArea.value = text;
+    sidebarTextArea.style.display = 'block';
+    // To put the text in the right place
+    var iframe = document.getElementById("mytextarea_ifr");
+    var elmnt = iframe.contentWindow.document.getElementById("tinymce");
+    // elmnt.style.display = "none";
+
+
+    // const classDiv = document.getElementById('tinymce');
+    const p = iframe.contentWindow.document.getElementsByTagName("p")[0];
+    p.innerHTML = text;
+    // elmnt.appendChild(p);
+    // const para = document.getElementById("tinymce").lastElementChild;
+    console.log(p);
+    // classDiv.appendChild(sidebarTextArea);
+    // classDiv.insertBefore(sidebarTextArea, classDiv.firstElementChild);
+
+
+    // sidebarTextArea.nodeTextContent;
+    
+    // show the sidebar
+    const offcanvas = new bootstrap.Offcanvas(document.getElementById('offcanvasExample'));
+    offcanvas.show();
+
+    // initialize TinyMCE on the text area
+    tinymce.init({
+      selector: '#mytextarea',
+      height: 300,
+      plugins: [
+        'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
+        'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
+        'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
+      ],
+      toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ' +
+        'alignleft aligncenter alignright alignjustify | ' +
+        'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help',
+      setup: editor => {
+        // listen for changes in the editor content and update the original element
+        editor.on('change', () => {
+          const newContent = editor.getContent();
+          element.textContent = newContent;
+        });
+      }
+    });
+  });
+});
+
+
+
+// // get all the paragraphs and headers on the page
+// const elements = document.querySelectorAll('p, h1, h2, h3, h4, h5, h6');
+// // console.log(elements);
+
+// // add an event listener to each element
+// elements.forEach(element => {
+//   element.addEventListener('click', () => {
+//     const text = element.textContent; // get the text content of the clicked element
+// console.log(text);
+//     // open the sidebar and populate it with the text content
+//     // you can use the same code we used earlier to create the sidebar and populate it with content
+//     // here, we'll assume you've stored the sidebar element in a variable called "sidebar"
+//     // const sidebarBody = sidebar.querySelector('.offcanvas-body');
+//     const sidebar = document.querySelector('#offcanvasExample');
+//     const sidebarBody = sidebar.classList.toggle('show');
+//     sidebarBody.innerHTML = `<textarea id="editor">${text}</textarea>`;
+//     console.log(sidebarBody);
+
+//         // initialize TinyMCE on the text area
+//     tinymce.init({
+//       selector: '#editor',
+//       height: 200,
+//       plugins: [
+//         'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
+//         'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
+//         'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
+//       ],
+//       toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ' +
+//         'alignleft aligncenter alignright alignjustify | ' +
+//         'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help',
+//       setup: editor => {
+//         // listen for changes in the editor content and update the original element
+//         editor.on('change', () => {
+//           const newContent = editor.getContent();
+//           element.textContent = newContent;
+//         });
+//       }
+//     });
+
+//     // show the sidebar
+//     const offcanvas = new bootstrap.Offcanvas(sidebar);
+//     offcanvas.show();
+//   });
+// });
+
+
+// // To have border around the Element on hover
+// window.addEventListener("mouseover", (e) => {
+
+//   if (!e.target) return
+  
+//   let element = e.target
+
+//   element.style.border = "1px solid red";
+
+//   // add border to the element that is hovered
+//   // let element = document.getElementById(`${e.target.id}`);
+
+//   // element.style.border = "1px solid red";
+// });
+
+// window.addEventListener("mouseout", (e) => {
+//   if (!e.target) return
+  
+//   let element = e.target
+
+//   element.style.border = "0px solid red";
+
+// });
+
+// To make it active and working
+tinymce.init({
+  selector: '#mytextarea',
+  height: 300,
+  plugins: [
+    'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
+    'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
+    'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
+  ],
+  toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ' +
+    'alignleft aligncenter alignright alignjustify | ' +
+    'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help',
+  setup: editor => {
+    // listen for changes in the editor content and update the original element
+    editor.on('change', () => {
+      const newContent = editor.getContent();
+      element.textContent = newContent;
+    });
+  }
+});
+
+
